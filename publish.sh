f412ff1a7b946f2752d852356b6219f1306edb57
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-09-08 19:40:24 +0200

    ci: publish script

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+set -e
+
+BASE_TAG="jedrzejlewandowski/templated"
+VERSION="1.0.0"
+
+git tag "v${VERSION}"
+git push origin "v${VERSION}"
+
+docker build -t "${BASE_TAG}" .
+docker tag "${BASE_TAG}" "${BASE_TAG}:latest"
+docker tag "${BASE_TAG}" "${BASE_TAG}-alpine:latest"
+docker tag "${BASE_TAG}" "${BASE_TAG}:${VERSION}"
+docker tag "${BASE_TAG}" "${BASE_TAG}-alpine:${VERSION}"
+docker push "${BASE_TAG}:latest"
+docker push "${BASE_TAG}-alpine:latest"
+docker push "${BASE_TAG}:${VERSION}"
+docker push "${BASE_TAG}:-alpine${VERSION}"
