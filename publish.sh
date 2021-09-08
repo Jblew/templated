128fd8d18df89cf564690c136a0a6b1a9cf28d0f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-09-08 19:44:24 +0200

    ci: fix: proper docker artifact labels

--- publish.sh
+++ publish.sh
@@ -4,17 +4,15 @@ cd "${DIR}"
 set -e
 
 BASE_TAG="jedrzejlewandowski/templated"
-VERSION="1.0.1"
+VERSION="1.0.2"
 
 git tag "v${VERSION}"
 git push origin "v${VERSION}"
 
 docker build -t "${BASE_TAG}" .
 docker tag "${BASE_TAG}" "${BASE_TAG}:latest"
-docker tag "${BASE_TAG}" "${BASE_TAG}-alpine:latest"
 docker tag "${BASE_TAG}" "${BASE_TAG}:${VERSION}"
-docker tag "${BASE_TAG}" "${BASE_TAG}-alpine:${VERSION}"
+docker tag "${BASE_TAG}" "${BASE_TAG}:${VERSION}-alpine"
 docker push "${BASE_TAG}:latest"
-docker push "${BASE_TAG}-alpine:latest"
 docker push "${BASE_TAG}:${VERSION}"
-docker push "${BASE_TAG}-alpine:${VERSION}"
+docker push "${BASE_TAG}:${VERSION}-alpine"
