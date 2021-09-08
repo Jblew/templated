1fe518f4115bf8f20c6cc2a192883ede283f94f7
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-09-08 18:20:31 +0200

    feat: configurable templating

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"text/template"
@@ -9,34 +10,58 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type ServeConfig struct {
+	Pages []PageConfig `json:"pages"`
+}
+
+type PageConfig struct {
+	URL      string `json:"url"`
+	Template string `json:"template"`
+}
+
+type TemplateData struct {
+	Params map[string]string
+}
+
 var templates = template.Must(template.ParseGlob("templates/*"))
 
 func main() {
+	config := loadConfig()
 	staticRoute := "/static/"
 	staticFileServer := http.FileServer(http.Dir("./static"))
 	http.Handle(staticRoute, http.StripPrefix(staticRoute, staticFileServer))
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", handleRoot)
-	r.HandleFunc("/role/{name}", handleRole)
+	r := buildMux(config)
 	http.Handle("/", r)
 
 	log.Printf("Listening on port 80")
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
-func handleRoot(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "index", nil)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println(err)
-		return
+func makePageHandler(templateName string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		data := TemplateData{Params: params}
+		err := templates.ExecuteTemplate(w, templateName, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
 	}
 }
 
-func handleRole(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "name: %v\n", vars["name"])
+func buildMux(config ServeConfig) *mux.Router {
+	r := mux.NewRouter()
+	for _, page := range config.Pages {
+		r.HandleFunc(page.URL, makePageHandler(page.Template))
+	}
+	return r
+}
+
+func loadConfig() ServeConfig {
+	bytes, _ := ioutil.ReadFile("serve.json")
+	var config ServeConfig
+	json.Unmarshal(bytes, &config)
+	return config
 }
